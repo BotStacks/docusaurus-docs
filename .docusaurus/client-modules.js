52a35c48f1e4e60d997261b364229d653e290bcc
Conner Jensen <connerjensen24@gmail.com>
2025-06-24 16:40:08 -0700

    remove node_modules and .docusaurus from git

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/pushpitjain/Desktop/docs2/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/pushpitjain/Desktop/docs2/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/pushpitjain/Desktop/docs2/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/pushpitjain/Desktop/docs2/docs/src/css/custom.css"),
-  require("/Users/pushpitjain/Desktop/docs2/docs/node_modules/docusaurus-plugin-image-zoom/lib/zoom"),
-];
